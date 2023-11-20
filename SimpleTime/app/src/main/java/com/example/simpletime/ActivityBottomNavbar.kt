247b1e27b4f241638dc4b8604daaa72d3f5cef6b
Adix-null <adomas@stalas.lt>
2023-11-20 20:59:12 +0200

    Improved navbar and fixed ui imperfections

---
+++ SimpleTime/app/src/main/java/com/example/simpletime/ActivityBottomNavbar.kt
@@ -0,0 +1,46 @@
+package com.example.simpletime
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_bottom_navbar.*
+
+class ActivityBottomNavbar : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view = inflater.inflate(R.layout.fragment_bottom_navbar, container, false)
+
+        val buttonList = listOf<Button>(view.findViewById(R.id.buttonHome), view.findViewById(R.id.buttonSaved), view.findViewById(R.id.buttonSearch), view.findViewById(R.id.buttonUpload))
+        val activityList = listOf(ActivityUserHome::class.java, ActivityComments::class.java, ActivityUserProfile::class.java, ActivityMyProfile::class.java)
+        val filledImageList = listOf(R.drawable.home_f, R.drawable.saved_f, R.drawable.search_glass_f, R.drawable.upload_f)
+        val blankImageList = listOf(R.drawable.home, R.drawable.saved, R.drawable.search_glass, R.drawable.upload)
+
+        for (i in buttonList.indices){
+            if(activity?.localClassName == activityList[i].simpleName){
+                buttonList[i].setBackgroundResource(filledImageList[i])
+            }
+            else{
+                buttonList[i].setOnClickListener{
+                    val intent = Intent(context, activityList[i])
+                    startActivity(intent);//overridePendingTransition(R.anim.enter_anim, R.anim.exit_anim)
+                }
+                buttonList[i].setBackgroundResource(blankImageList[i])
+            }
+        }
+
+
+
+
+
+        return view
+    }
+}
--- SimpleTime/app/src/main/java/com/example/simpletime/bottom_navbar.kt
+++
@@ -1,7 +0,0 @@
-package com.example.simpletime
-
-import androidx.fragment.app.Fragment
-
-class bottom_navbar : Fragment(R.layout.fragment_bottom_navbar) {
-
-}
